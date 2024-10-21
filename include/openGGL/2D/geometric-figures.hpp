8694a3474217cd4b015618bd95df1583c5cf1789
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-10-21 15:49:45 +0000

    docs: add license and author header to every source/header file

--- include/openGGL/2D/geometric-figures.hpp
+++ include/openGGL/2D/geometric-figures.hpp
@@ -1,3 +1,24 @@
+/***************************************************************************
+ * Project                                                ____  ____ _
+ *                                 ___  _ __   ___ _ __  / ___|/ ___| |
+ *                                / _ \| '_ \ / _ \ '_ \| |  _| |  _| |
+ *                               | (_) | |_) |  __/ | | | |_| | |_| | |___
+ *                                \___/| .__/ \___|_| |_|\____|\____|_____|
+ *                                     |_|
+ *
+ * Author: Luís Góis
+ *
+ * This software is licensed as described in the file LICENSE, which
+ * you should have received as part of this distribution.
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell
+ * copies of the Software, and permit persons to whom the Software is
+ * furnished to do so, under the terms of the LICENSE file.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ***************************************************************************/
 #ifndef INCLUDE_2D_GEOMETRIC_FIGURES_H_
 #define INCLUDE_2D_GEOMETRIC_FIGURES_H_
 
